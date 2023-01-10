025e59f5a3e228d657db4b58083ff74c70a49d10
Заид Мингалиев <MingalievZZ@stud.kai.ru>
2023-01-10 18:48:04 +0300

    Add array

--- hello.go
+++ hello.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var msg string
-	msg = "Hello Go!"
-	fmt.Println(msg)
+	// var msg string
+	// msg = "Hello Go!"
+
+	// age := "25"
+
+	// var numbers [5]int = [5]int{1, 2, 3, 4, 5}
+	// fmt.Println(msg + age)
+
+	numbers := [...]int{1, 2, 3, 4, 5}
+
+	fmt.Println(numbers)
+	fmt.Println(numbers[2])
 }
