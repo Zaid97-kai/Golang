b642869141e0f412ea5264dd74def36a01f3a61a
Заид Мингалиев <MingalievZZ@stud.kai.ru>
2023-01-10 21:16:19 +0300

    Add function

--- hello.go
+++ hello.go
@@ -3,26 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// var msg string
-	// msg = "Hello Go!"
+	var age, name = add(4, 5, "Tom", "Simpson")
+	fmt.Println(age)  // 9
+	fmt.Println(name) // Tom Simpson
+}
 
-	// age := "25"
-
-	// var numbers [5]int = [5]int{1, 2, 3, 4, 5}
-	// fmt.Println(msg + age)
-
-	numbers := [...]int{1, 2, 3, 4, 5}
-
-	fmt.Println(numbers)
-	fmt.Println(numbers[2])
-
-	for i := 1; i < 10; i++ {
-		fmt.Println(i * i)
-	}
-
-    var users = [3]string{"Tom", "Alice", "Kate"}
-    for index, value := range users
-    {
-        fmt.Println(index, value)
-    }
+func add(x, y int, firstName, lastName string) (int, string) {
+	var z int = x + y
+	var fullName = firstName + " " + lastName
+	return z, fullName
 }
